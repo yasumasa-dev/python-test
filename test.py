3d46c99cd42e3150231c550a48c5d6efcb1b8be0
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-03 21:31:54 +0900

    追加学習

--- test.py
+++ test.py
@@ -1,32 +1,71 @@
 # -*- coding: utf-8 -*- Pythonを日本語に対応
 
-# print("Hello World")
+#Comment
+# print("Hello World!")
 
 # print("Hello")
-# print("World")
 
+# print("World!")
+
+
+#演算
 # print(1+1)
 # print(1-1)
 # print(2*2)
 # print(10/2)
-# print(5%2)
+# print(5%3)
 
-# unko = 'こんにちは'
+#変数
+# unko = 'l_size'
+# unko_length = 0
+# unko_times = 5.5
 
-# print(unko)
+# print(unko_length + unko_times)
 
-###with
+#条件分岐と関係演算子
+# if else elif
+# ==, !=, <, >, >=, <=
+# if unko_length > 6:
+# 	print('ooi')
+# elif unko_length == 0:
+# 	print('nai!')
+# else:
+# 	print('sukunai')
+
+# 関数
+# def unko_funbaru(arg):
+# 	unko_status = arg
+
+# 	if(unko_status < 10):
+# 		return 'mada daijobu'
+# 	else:
+# 		return 'yabai'
+
+# print()
+
+#list
+# unko_list = ['unko_small', 'unko_medium', 'unko_large']
+# print(unko_list[0])
+
+#for
+# for index in range(11):
+# 	print(unko_funbaru(index))
+
+# for item in unko_list:
+# 	print(item)
+
+#with
 # open()
-# with open('./unko.txt','r') as file:
-#   print(file.read())
+# with open('./unko.txt', 'r') as file:
+# 	print(file.read())
 
-###class
+#class
 # class Card:
-#   def __init__(self, date, user_name):
-#     self.date = date
-#     self.user_name = user_name
-#   def message(self):
-#     return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+# 	def __init__(self, date, user_name):
+# 		self.date = date
+# 		self.user_name = user_name
+# 	def message(self):
+# 		return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
 
 # date_a = '2020-01-01'
 # user_name_a = 'Taro'
@@ -36,19 +75,14 @@
 # user_name_b = 'Kayoko'
 # card_b = Card(date_b, user_name_b)
 
-# print(card_b.message()) 
+# print(card_b.message())
 
-###import
-import math
-print(math.pi)
-
-#外部モジュール
-#Numpy
+#import
+# import math
+# print(math.pi)
 
 import numpy
 
-numpy_list = [3,1,5,10,2093,304,123]
+numpy_list = [3, 1, 5, 10, 2093, 304, 123]
 print(numpy.sum(numpy_list))
-
-
 
